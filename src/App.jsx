8f84e2ea4ff97010abd3c0b05abcbf20dd41bf69
andreas2897 <andreas.2897@gmail.com>
2020-04-26 19:46:32 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./views/components/Button/Button.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>app js</h1>
+      <div className="custom-btn custom-btn-outlined">AAA</div>
+      <div className="custom-btn custom-btn-contained">AAA</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ButtonUI = ({type, children}) => {
+
+    if ( type = "outlined"){
+        return <div className="custom"></div>
+    } else if ( type = "contained"){
+        return
+    } else if ( type = "textual"){
+        return
+    } else {
+    return <div>buttonUI</div>
+}
+}
+export default ButtonUI
